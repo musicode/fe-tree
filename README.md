0a7588107714dd65e46956ef726ffafa734204fb
musicode <musicode@qq.com>
2015-11-30 11:34:04 +0800

    update

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # resource-tree
 
-使用方式
+## 节点用法
 
 ```javascript
-var node = new Node('file');
+var node = Node.create('filePath');
 node.walk({
+    // 是否递归遍历
     recursive: true,
+    // 扩展 html 中的依赖提取规则
     htmlRules: [
         {
             pattern: //g,
@@ -17,6 +19,7 @@ node.walk({
             }
         }
     ],
+    // 扩展 css 中的依赖提取规则
     cssRules: [
         {
             pattern: //g,
@@ -28,13 +31,113 @@ node.walk({
             }
         }
     ],
+    // AMD require config 配置
     amdConfig: {
         baseUrl: '',
+        paths: {
+
+        },
         packages: [
 
         ]
     },
+    // 处理每一个依赖
+    processDependency: function (dependency, node) {
 
+        // 对于不合法的依赖，需要进行过滤
+        // 过滤是通过 return 实现的，即不 return 或 return null 表示过滤
+
+        // 纠正依赖的路径通过改写 dependency.raw 实现
+        // 如果是 amd 插件，会传入 dependency.plugin，可改写该属性
+        // 例如，对于一个模板文件，如果希望打包成 amd 模块，可分为两步：
+        // 1. 把 dependency.raw 改写成符合 amd 命名规则的名字
+        // 2. 把 dependency.plugin 改写为 ''
+
+
+    }
 });
 ```
 
+var projectDir = '/Users/zhujl/github/marketing';
+
+exports.parse({
+    files: [
+        projectDir + '/couponAdd.html'
+    ],
+    amdConfig: {
+        baseUrl: projectDir + '/src',
+        packages: [
+            {
+                name: 'cc',
+                location: '../dep/cc/1.0.0/src',
+                main: 'main'
+            },
+            {
+                name: 'moment',
+                location: '../dep/moment/2.10.6/src',
+                main: 'moment'
+            }
+        ]
+    },
+    processDependency: function (dependency, node) {
+
+        var raw = dependency.raw;
+
+        // 过滤依赖
+        if (/[{}$]/.test(raw)) {
+            return;
+        }
+
+        // 纠正依赖路径
+        if (!dependency.amd) {
+
+            var prefix = {
+                'src': projectDir + '/',
+                'dep': projectDir + '/',
+                '/src': projectDir,
+                '/dep': projectDir,
+                'common': projectDir + '/src/'
+            };
+
+            util.each(
+                prefix,
+                function (value, key) {
+                    if (raw.startsWith(key)) {
+                        dependency.file = value + raw;
+                        return;
+                    }
+                }
+            );
+
+            if (!dependency.file && /^(\.\/|[^./])/.test(raw)) {
+                dependency.file = path.join(node.file, '..', raw);
+            }
+
+        }
+
+        console.log(node.file);
+        console.log(dependency);
+        console.log('-----------------------------------')
+
+        var moduleExclude = {
+            jquery: 1,
+            text: 1,
+            tpl: 1,
+            css: 1,
+            js: 1
+        };
+
+        var rawExclude = {
+            require: 1,
+            exports: 1,
+            module: 1
+        };
+
+        if (!moduleExclude[dependency.module]
+            && !rawExclude[dependency.raw]
+        ) {
+            return dependency;
+        }
+
+    }
+});
--- index.js
+++ index.js
@@ -3,8 +3,6 @@
  * @author musicode
  */
 
-var path = require('path');
-
 var Node = require('./lib/Node');
 var util = require('./lib/util');
 
@@ -14,7 +12,7 @@ var util = require('./lib/util');
  * @inner
  * @type {Object}
  */
-var dependencyMap = { };
+exports.dependencyMap = { };
 
 /**
  * 反向依赖表（一个资源被哪些资源依赖）
@@ -22,30 +20,7 @@ var dependencyMap = { };
  * @inner
  * @type {Object}
  */
-var reverseDependencyMap = { };
-
-/**
- * 把两个 map 转成可输出的格式
- *
- * @param {Object|Array} obj
- * @return {Object|Array}
- */
-function toJSON(obj) {
-    var result = Array.isArray(obj) ? [ ] : { };
-    util.each(
-        obj,
-        function (value, key) {
-            if (value instanceof Node) {
-                value = value.toJSON();
-            }
-            else if (Array.isArray(value)) {
-                value = toJSON(value);
-            }
-            result[ key ] = value;
-        }
-    );
-    return result;
-}
+exports.reverseDependencyMap = { };
 
 /**
  * 分析入口文件，生成正反两棵依赖树
@@ -67,12 +42,12 @@ exports.parse = function (options) {
 
     var processFile = function (file) {
 
-        if (dependencyMap[ file ]) {
+        if (exports.dependencyMap[ file ]) {
             return;
         }
 
         var node = Node.create(file);
-        dependencyMap[ file ] = node;
+        exports.dependencyMap[ file ] = node;
 
         node.walk({
             htmlRules: htmlRules,
@@ -92,9 +67,9 @@ exports.parse = function (options) {
 
                     node.addChild(child);
 
-                    var parents = reverseDependencyMap[ file ];
+                    var parents = exports.reverseDependencyMap[ file ];
                     if (!Array.isArray(parents)) {
-                        parents = reverseDependencyMap[ file ] = [ ];
+                        parents = exports.reverseDependencyMap[ file ] = [ ];
                     }
 
                     if (parents.indexOf(node.file) < 0) {
@@ -115,94 +90,15 @@ exports.parse = function (options) {
 
     files.forEach(processFile);
 
-    util.writeJSON(__dirname + '/dependencyMap.json', toJSON(dependencyMap));
-    util.writeJSON(__dirname + '/reverseDependencyMap.json', toJSON(reverseDependencyMap));
-
 };
 
-
-var projectDir = '/Users/zhujl/github/marketing';
-
-exports.parse({
-    files: [
-        projectDir + '/couponAdd.html'
-    ],
-    amdConfig: {
-        baseUrl: projectDir + '/src',
-        packages: [
-            {
-                name: 'cc',
-                location: '../dep/cc/1.0.0/src',
-                main: 'main'
-            },
-            {
-                name: 'moment',
-                location: '../dep/moment/2.10.6/src',
-                main: 'moment'
-            }
-        ]
-    },
-    processDependency: function (dependency, node) {
-
-        var raw = dependency.raw;
-
-        // 过滤依赖
-        if (/[{}$]/.test(raw)) {
-            return;
-        }
-
-        // 纠正依赖路径
-        if (!dependency.amd) {
-
-            var prefix = {
-                'src': projectDir + '/',
-                'dep': projectDir + '/',
-                '/src': projectDir,
-                '/dep': projectDir,
-                'common': projectDir + '/src/'
-            };
-
-            util.each(
-                prefix,
-                function (value, key) {
-                    if (raw.startsWith(key)) {
-                        dependency.file = value + raw;
-                        return;
-                    }
-                }
-            );
-
-            if (!dependency.file && /^(\.\/|[^./])/.test(raw)) {
-                dependency.file = path.join(node.file, '..', raw);
-            }
-
-        }
-
-        console.log(node.file);
-        console.log(dependency);
-        console.log('-----------------------------------')
-
-        var moduleExclude = {
-            jquery: 1,
-            text: 1,
-            tpl: 1,
-            css: 1,
-            js: 1
-        };
-
-        var rawExclude = {
-            require: 1,
-            exports: 1,
-            module: 1
-        };
-
-        if (!moduleExclude[dependency.module]
-            && !rawExclude[dependency.raw]
-        ) {
-            return dependency;
-        }
-
-    }
-});
-
+/**
+ * 持久化 json
+ *
+ * @param {string} file
+ * @param {Object} map
+ */
+exports.writeJSON = function (file, map) {
+    util.writeJSON(file, Node.toJSON(map));
+};
 
